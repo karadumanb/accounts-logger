b98a861e89f0e56a13a846b35ffa0ea4ce939e5b
karadumanb <karadumanbaturay@gmail.com>
2018-02-27 20:21:33 +0300

    Entire project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyAuB6YcRdna9GFX0Kjkb9tG1zUeGy9EcPw",
+    authDomain: "clientpanelprojectofbaturay.firebaseapp.com",
+    databaseURL: "https://clientpanelprojectofbaturay.firebaseio.com",
+    projectId: "clientpanelprojectofbaturay",
+    storageBucket: "clientpanelprojectofbaturay.appspot.com",
+    messagingSenderId: "569026113933"
+  }
+};
